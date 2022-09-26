c05591061657019ead6d0c006f8c423a4624bd2a
lukis76 <kiu.manoguerra@gmail.com>
2022-09-26 17:42:58 -0300

    update rutes

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -1,27 +1,46 @@
 const { Router } = require('express')
 const {
-  apiSearchRecipe,
-  dbSearchRecipe,
-  apiSearchID,
+  // apiSearchRecipe,
+  // dbSearchRecipe,
+  // apiSearchID,
+  getAllInfo,
+  getInfoName,
 } = require('./utils/index.js')
 
 const router = Router()
 
 router.get('/', async (req, res) => {
   const { name } = req.query
-
-  const apiResult = await apiSearchRecipe(name)
-  const dbResult = await dbSearchRecipe(name)
-
-  if (apiResult.totalResults === 0 && !dbResult) {
-    return res.status(400).send(`no se an encontrado resultadors para ${name}`)
+  try {
+    if (name) {
+      const infoName = await getInfoName(name)
+      if (infoName !== 'error') {
+        infoName.length > 0
+          ? res.json(infoName)
+          : res.status(400).json({ msg: 'recipes not found' })
+      } else {
+        res.status(404).json({ msg: 'Error' })
+      }
+    } else {
+      const allData = await getAllInfo()
+      if (allData !== error) {
+        res.json(allData)
+      } else {
+        res.status(404).json({ msg: 'Error en la busqueda de datos' })
+      }
+    }
+  } catch (error) {
+    return res.status(500).send('Error Inesperado')
   }
 
-  console.log(
-    '🚀 ~ file: recipe.js ~ line 19 ~ router.get ~ apiResult',
-    apiResult
-  )
-  res.send(apiResult)
+  // const apiResult = await apiSearchRecipe(name)
+  // const dbResult = await dbSearchRecipe(name)
+
+  // if (apiResult.totalResults === 0 && !dbResult) {
+  //   return res.status(400).send(`no se an encontrado resultadors para ${name}`)
+  // }
+
+  // res.send(apiResult)
 
   // return res.send(`total de resetas en contradas ${apiResult.totalResults + dbResult.length}`)
 })
--- api/src/routes/utils/index.js
+++ api/src/routes/utils/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const axios = require('axios')
-const { Recipe } = require('../../db')
+const e = require('express')
+const { Recipe, Diet } = require('../../db')
 
 const { API_KEY, API_URL_ONE, API_URL_TWO, API_EXTRA } = process.env
 console.log('🚀 ~ file: apiInfo.js ~ line 5 ~ API_URL_ONE', API_URL_ONE)
@@ -9,60 +10,202 @@ console.log('🚀 ~ file: apiInfo.js ~ line 6 ~ API_KEY', API_KEY)
 // API_URL_TWO=https://api.spoonacular.com/recipes/
 // API_EXTRA='&addRecipeInformation=true&number=100'
 
-const apiAllSearchRecipe = async () => {
-  const result = await axios.get(`${API_URL_ONE}${API_KEY}${API_EXTRA}`)
-  const apiInfo = result.data.results.map(el => {
-    return {
-      name: el.title,
-      img: el.image,
-      id: el.id,
-      diets: el.diets,
-      healthScore: el.healthScore,
-      dishType: el.dishType,
-      summary: el.summary,
-      instructions: el.analyzedInstructions
+// const apiAllSearchRecipe = async () => {
+//   const result = await axios.get(`${API_URL_ONE}${API_KEY}${API_EXTRA}`)
+//   const apiInfo = result.data.results.map((el) => {
+//     return {
+//       name: el.title,
+//       img: el.image,
+//       id: el.id,
+//       diets: el.diets,
+//       healthScore: el.healthScore,
+//       dishType: el.dishType,
+//       summary: el.summary,
+//       instructions: el.analyzedInstructions,
+//     }
+//   })
+//   return apiInfo
+// }
+
+// const apiSearchRecipe = async (name) => {
+//   const result = await axios.get(
+//     `${API_URL_ONE}query=${name}&${API_KEY}${API_EXTRA}`
+//   )
+//   const apiInfo = result.data.results.map((el) => {
+//     return {
+//       name: el.title,
+//       vegetarian: el.vegetarian,
+//       vegan: el.vegan,
+//       glutenFree: el.glutenFree,
+//       dairyFree: el.dairyFree,
+//       img: el.image,
+//       apiID: el.id,
+//       score: el.spoonacularScore,
+//       healthScore: el.healthScore,
+//       dishType: el.dishTypes?.map((el) => el),
+//       diets: el.diets?.map((el) => el),
+//       summary: el.summary,
+//       // instructions: el.analyzedInstructions
+//     }
+//   })
+//   return apiInfo
+// }
+
+// const dbSearchRecipe = async (name) => {
+//   const result = await Recipe.findAll({
+//     where: {
+//       name: name,
+//     },
+//   })
+
+//   return result
+// }
+
+// const apiSearchID = async (id) => {
+//   const result = await axios.get(`${API_URL_TWO}${id}/information?${API_KEY}`)
+
+//   return result.data
+// }
+
+const getApiInfo = async () => {
+  try {
+    const resAxio = await axios.get(`${API_URL_ONE}${API_KEY}${API_EXTRA}`)
+    const { results } = resAxio.data
+
+    if (results.length > 0) {
+      let result = results?.map((el) => {
+        return {
+          name: el.title,
+          vegetarian: el.vegetarian,
+          vegan: el.vegan,
+          glutenFree: el.glutenFree,
+          dairyFree: el.dairyFree,
+          img: el.image,
+          apiID: el.id,
+          score: el.spoonacularScore,
+          healthScore: el.healthScore,
+          dishType: el.dishTypes?.map((el) => el),
+          diets: el.diets?.map((el) => el),
+          summary: el.summary,
+          steps: el.analyzedInstructions,
+        }
+      })
+      return result
     }
-  })
-  return apiInfo
+  } catch (err) {
+    console.error(err)
+    return []
+  }
 }
 
-const apiSearchRecipe = async (name) => {
-  const result = await axios.get(`${API_URL_ONE}query=${name}&${API_KEY}${API_EXTRA}`)
-  const apiInfo = result.data.results.map(el => {
-    return {
-      name: el.title,
-      img: el.image,
-      id: el.id,
-      diets: el.diets,
-      healthScore: el.healthScore,
-      dishType: el.dishType,
-      summary: el.summary,
-      instructions: (result.analyzedInstructions[0] && result.analyzedInstructions[0].steps?result.analyzedInstructions[0].steps.map(item=>item.step).join(" \n"):'')
+const getAllInfo = async () => {
+  try {
+    const apiInfo = await getApiInfo()
+    const dbInfo = await getDbInfo()
+
+    const result = apiInfo.concat(dbInfo)
+
+    return result
+  } catch (err) {
+    console.error(err)
+    return 'error'
+  }
+}
+
+const getApiName = async (name) => {
+  try {
+    const resAxio = await axios.get(
+      `${API_URL_ONE}query=${name}${API_EXTRA}&${API_KEY}`
+    )
+    const { results } = resAxio.data
+
+    if (results.length > 0) {
+      let result = results?.map((el) => {
+        return {
+          name: el.title,
+          vegetarian: el.vegetarian,
+          vegan: el.vegan,
+          glutenFree: el.glutenFree,
+          dairyFree: el.dairyFree,
+          img: el.image,
+          apiID: el.id,
+          score: el.spoonacularScore,
+          healthScore: el.healthScore,
+          dishType: el.dishTypes?.map((el) => el),
+          diets: el.diets?.map((el) => el),
+          summary: el.summary,
+          steps: el.analyzedInstructions,
+        }
+      })
+      return result
+    } else {
+      console.log('No hay coinsidencias en la api')
     }
-  })
-  return apiInfo
+  } catch (err) {
+    console.error(err)
+    return 'error'
+  }
 }
 
-const dbSearchRecipe = async (name) => {
-  const result = await Recipe.findAll({
-    where: {
-      name: name,
-    },
-  })
-
-  return result
+const getDbInfo = async () => {
+  try {
+    const dbData = await Recipe.findAll({
+      include: {
+        model: Diet,
+        attributes: ['name'],
+        through: {
+          attributes: [],
+        },
+      },
+    })
+    let res = await dbData?.map((el) => {
+      return {
+        id: el.id,
+        name: el.name,
+        summary: el.summary,
+        score: el.score,
+        healthScore: el.healthScore,
+        img: el.img,
+        steps: el.steps,
+        diets: el.diets?.map((el) => el.name),
+      }
+    })
+    return res
+  } catch (err) {
+    console.error(err)
+    return 'error'
+  }
 }
-// n:n
 
-const apiSearchID = async (id) => {
-  const result = await axios.get(`${API_URL_TWO}${id}/information?${API_KEY}`)
+const getDbName = async (name) => {
+  try {
+    const dbInfo = await getDbInfo()
+    const filterName = dbInfo.filter((el) => el.name.includes(name))
+    return filterName
+  } catch (err) {
+    return 'error'
+  }
+}
 
-  return result.data
+const getInfoName = async (name) => {
+  try {
+    const apiName = await getApiName(name)
+    const dbName = await getDbName(name)
+
+    return apiName.concat(dbName)
+  } catch (err) {
+    return 'error'
+  }
 }
 
 module.exports = {
-  apiAllSearchRecipe,
-  apiSearchRecipe,
-  dbSearchRecipe,
-  apiSearchID,
+  //-----------------
+  getInfoName,
+  getAllInfo,
+
+  //-----------------
+  // apiAllSearchRecipe,
+  // apiSearchRecipe,
+  // dbSearchRecipe,
+  // apiSearchID,
 }
