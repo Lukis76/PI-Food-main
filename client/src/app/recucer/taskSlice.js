fc834f495773c87bd2e9e3faea4e024283183b79
lukis76 <kiu.manoguerra@gmail.com>
2022-09-27 12:42:36 -0300

    action + refactor + modularizacion

--- client/src/app/recucer/taskSlice.js
+++ client/src/app/recucer/taskSlice.js
@@ -1,9 +1,11 @@
-import axios from 'axios'
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  loading: false,
   data: [],
+  recipes: [],
+  recipesAll: [],
+  types: [],
+  details: [],
 }
 
 export const taskSlice = createSlice({
@@ -19,10 +21,3 @@ export const taskSlice = createSlice({
 export const { setTasks } = taskSlice.actions
 
 export default taskSlice.reducer
-
-export const fetchRecipe = (name) => (dispatch) => {
-  axios
-    .get(`http://localhost:3088/recipes?name=${name}`)
-    .then((res) => dispatch(setTasks(res.data)))
-    .catch((err) => console.error(err))
-}
