1bf0cf03e7fe8535d915a55a97b5f9fc96cf6246
lukis76 <kiu.manoguerra@gmail.com>
2022-09-27 12:36:56 -0300

    reducer

--- client/src/app/recucer/taskSlice.js
+++ client/src/app/recucer/taskSlice.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   loading: false,
