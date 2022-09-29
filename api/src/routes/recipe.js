12a0b858275345288130da337619ee5d5ab4cb1a
lukis76 <kiu.manoguerra@gmail.com>
2022-09-29 12:41:59 -0300

    post recipe

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { getAllInfo, getInfoName } = require('./utils/index.js')
+const { Recipe, diet, Op } = require('../db')
 
 const router = Router()
 
@@ -26,6 +27,57 @@ router.get('/', async (req, res) => {
   } catch (error) {
     return res.status(500).send('Error Inesperado')
   }
+})
+
+router.post('/', async (req, res) => {
+  const {
+    name,
+    summary,
+    healthScore,
+    img,
+    steps,
+    diets,
+    glutenFree,
+    dairyFree,
+    vegan,
+    vegetarian,
+  } = req.body
+
+  try {
+    const createRecipe = await Recipe.create({
+      name,
+      summary,
+      healthScore,
+      img,
+      steps,
+      diets,
+      glutenFree,
+      dairyFree,
+      vegan,
+      vegetarian,
+    })
+
+    const dietDB = await Diet.findAll({
+      where: {
+        name: diets,
+      },
+    })
+    if (!name) {
+      return res
+        .status(400)
+        .send({ msg: 'Deve ingresar el name para la receta' })
+    }
+    if (!summary) {
+      return res
+        .status(400)
+        .send({ msg: 'Deve ingresar el summary para la receta' })
+    }
+
+    createRecipe.addDiet(dietDB)
+
+    res.send("Successfully created a new Recipe !")
+
+  } catch (error) {}
 })
 
 router.get('/:RecipeID', async (req, res) => {
