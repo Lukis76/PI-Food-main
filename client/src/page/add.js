e877fc88bebd7812140b47e1e72ca69b61914528
lukis76 <kiu.manoguerra@gmail.com>
2022-10-01 09:36:43 -0300

    refactor add recipe

--- client/src/page/add.js
+++ client/src/page/add.js
@@ -1,10 +1,10 @@
 // eslint-disable
 
 import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 import { center } from '../style/shorcuts'
-import { Link, useHistory, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { postRecipe } from '../app/actions/postRecipe'
 import { AddDiets } from '../components/add/aadDiets'
 import { AddName } from '../components/add/addName'
@@ -15,15 +15,21 @@ import { AddImg } from '../components/add/addImg'
 export const Add = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const diets = useSelector((state) => state.diets)
-const [error, setError] = useState({
-  name: false,
-  summary: false,
-  healthScore: false,
-  steps: false,
-  img: false,
-  diets: false,
-})
+
+  const [error, setError] = useState({
+    name: false,
+    validName: '',
+    summary: false,
+    validSummary: '',
+    healthScore: false,
+    validHealthScore: '',
+    steps: false,
+    validSteps: '',
+    img: false,
+    validImg: '',
+    diets: false,
+    validDiets: '',
+  })
   const [newRecipe, setNewRecipe] = useState({
     name: '',
     summary: '',
@@ -31,41 +37,9 @@ const [error, setError] = useState({
     steps: [],
     img: '',
     diet: [],
-    createdb: true
+    createdb: true,
   })
   console.log('init => ', newRecipe)
-  const handleChenge = (e) => {
-    console.log('onChange')
-    if (e.target.name === 'name') {
-      setNewRecipe({ ...newRecipe, [e.target.name]: e.target.value })
-    }
-    if (e.target.name === 'summary') {
-      setNewRecipe({ ...newRecipe, [e.target.name]: e.target.value })
-    }
-    if (e.target.name === 'healthScore') {
-
-      // const result = {...newRecipe, [e.target.name]: e.target.value}
-      setNewRecipe({...newRecipe, [e.target.name]: e.target.value})
-    }
-    // if (e.target.name === 'img') {
-    //   setNewRecipe((state) => {
-    //     state = { ...newRecipe, [e.target.name]: e.target.value }
-    //   })
-    // }
-    // if (e.target.name === 'diet') {
-    //   setNewRecipe((state) => {
-    //     state = {
-    //       ...newRecipe,
-    //       [e.target.name]: [...newRecipe.diet, e.target.value],
-    //     }
-    //   })
-    // }
-    // if (e.target.name === 'step') {
-    //   setNewRecipe((state) => {
-    //     state = { ...newRecipe, [e.target.name]: e.target.value }
-    //   })
-    // }
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -77,23 +51,39 @@ const [error, setError] = useState({
     // steps: [],
     // img: '',
     // diet: [],})
-    // navigate('/home')
+    navigate('/home')
     console.log('submit ejecutado')
   }
 
   return (
     <ContentAdd>
       <form onSubmit={handleSubmit}>
-        <AddName />
-        <AddSummary />
-        <AddHealthScore />
-        <AddImg />
+        <AddName
+          setError={setError}
+          setNewRecipe={setNewRecipe}
+          error={error}
+        />
+        <AddSummary
+          setError={setError}
+          setNewRecipe={setNewRecipe}
+          error={error}
+        />
+        <AddHealthScore
+          setError={setError}
+          setNewRecipe={setNewRecipe}
+          newRecipe={newRecipe}
+        />
+        <AddImg
+          setError={setError}
+          setNewRecipe={setNewRecipe}
+          newRecipe={newRecipe}
+        />
         <label>
           <textarea
             name='step'
             cols='30'
             rows='10'
-            onChange={handleChenge}
+            // onChange={handleChenge}
           ></textarea>
         </label>
 
