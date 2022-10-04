32e68318de32b94f108483ed63cdc39efd8ab1da
lukis76 <kiu.manoguerra@gmail.com>
2022-10-04 15:42:58 -0300

    basura

--- client/src/app/actions/filterOrder.js
+++
@@ -1,5 +0,0 @@
-import { setFilterOrder } from '../recucer/taskSlice'
-
-export const filterOrder = (data) => (dispatch) => {
-  dispatch(setFilterOrder(data))
-}
--- client/src/app/recucer/taskSlice.js
+++ client/src/app/recucer/taskSlice.js
@@ -117,7 +117,6 @@ export const {
   setTasks,
   setGetRecipes,
   setFilterSearch,
-  setGetRecipesName,
   setGetRecipeID,
   setGetTypes,
   setPaguination,
