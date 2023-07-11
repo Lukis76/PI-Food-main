9c6a07c79c3aa8ab506e68a735b097eab7c42110
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-07-11 17:32:15 -0300

    type json

--- api/src/app.js
+++ api/src/app.js
@@ -16,6 +16,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
+  res.header('constet-type', 'application/json')
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
